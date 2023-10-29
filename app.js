5ea5cfe17693d33dc882779f0e52455e25997310
AlphaZer00 <batudasdemir@gmail.com>
2023-10-28 20:23:59 -0400

    Add manageMemory function

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ let num1;
 let num2;
 let operator;
 let clearButtonPressed = false;
+let memory = "";
+let key = "";
+let text = "";
 
 const display = document.querySelector(".display");
 const acButton = document.querySelector(".ac");
 const cButton = document.querySelector(".c");
 const mathButtons = document.querySelector(".math-buttons");
-const justButtons = mathButtons.childNodes;
+const equalsButton = document.querySelector(".equals");
 
 acButton.addEventListener("click", clearMem);
 cButton.addEventListener("click", clearDisplay);
+equalsButton.addEventListener("click", calculation);
 
 function add(num1, num2) {
     return num1 + num2;
@@ -35,57 +39,37 @@ function operate(num1, operator, num2) {
         return add(num1, num2);
     } else if (operator === "-") {
         return subtract(num1, num2); 
-    } else if (operator === "*") {
+    } else if (operator === "*" || operator === "x" || operator === "X") {
         return multiply(num1, num2);
     } else if (operator === "/" || operator === "÷") {
         return divide(num1, num2);
     }
 }
 
+//This function makes buttons update display
 mathButtons.addEventListener("click", (e) => {
-    let text = e.target.textContent;
-    if (text.length > 1) return;
-    display.textContent = display.textContent + text
+    text = e.target.textContent;
+    console.log(e);
+    manageMemory("m");
 });
-
 
 document.body.addEventListener("keydown", function(e) {
-    let key = e.key;
-    const regex = /[*0-9xX\/÷=+-\.%]/g;
-    if (display.textContent == "ERR: Divide by 0") {
-        return;
-    }
-    if (regex.test(key)) {
-        display.textContent=display.textContent+key;
-    }
-
-    if (key == "Backspace") {
-        display.textContent = display.textContent.slice(0, -1);
-    }
-    if (key === "=" || key === "Enter") {
-        calculation();
-    }
+    key = e.key;
+    manageMemory("k");
 });
 
-function createArray(string) {
-    let str = string;
-    const arr = str.split(/[+-\/*xX÷]/g);
-    return arr;
-}
-function getOperatorsArray() {
-    operator = display.textContent.match(/[\/*+\-xX÷]/g);
-    return operator;
-}
-
 function calculation() {
-    let myArray =createArray(display.textContent.slice(0, -1));
+    console.log(memory);
+    let myArray =createArray(memory);
+    console.log(myArray);
     if (clearButtonPressed == true) {
         myArray = [];
+        memory = '';
         clearButtonPressed = false;
-        display.textContent = myArray;
-        console.log(myArray);
+        display.textContent = myArray.toString();
         return;
-    }
+    } 
+
     let operatorsArray=getOperatorsArray();
     console.log(operatorsArray);
     let length = myArray.length;
@@ -95,9 +79,11 @@ function calculation() {
         console.log(operator);
         getNumberValues(myArray);
         simplifyArray(myArray);
+        memory = myArray[0];
+        console.log(myArray);
     }
     
-    display.textContent=Math.round(myArray[0] *1000) / 1000;
+    display.textContent=Math.round(myArray[0] * 1000) / 1000;
     if (display.textContent === "Infinity") {
         display.textContent = "ERR: Divide by 0";
     }
@@ -109,8 +95,8 @@ function getNumberValues(arr) {
 }
 
 function simplifyArray(arr) {
-    console.log(arr);
-    myArray =arr.splice(0, 2, operate(num1, operator, num2));
+    myArray = arr.splice(0, 2, operate(num1, operator, num2));
+    console.log(myArray);
     return myArray;
 }
 
@@ -120,10 +106,74 @@ function deleteFirstElement(arr) {
 }
 
 function clearMem() {
-    clearButtonPressed =true;
-    calculation();
+    memory = "";
+    display.textContent = "";
+    clearButtonPressed = false;
 }
 
 function clearDisplay() {
     display.textContent = "";
+}
+
+function createArray(string) {
+    let str = string;
+    const arr = str.split(/[+-\/*xX÷=]/g);
+    return arr;
+}
+
+function getOperatorsArray() {
+    operator = memory.match(/[\/*+\-xX÷]/g);
+    return operator;
+}
+
+function manageMemory(eventType) {
+    const isCalcButton = /[*0-9xX+\/÷=+\-.%]/;
+    const isNumber = /[0-9\.]/g;
+    const isOperator= /[\/*+\-xX÷]/g;
+    let operatorPressed = false;
+    let backspacePressed = false;
+    console.log(memory);
+    if (key === "Backspace") {
+       backspacePressed = true;
+    }
+
+    if (eventType === "k" && backspacePressed == true) {
+        memory = memory.toString().slice(0, -1);
+        display.textContent = (display.textContent).slice(0, -2);
+        backspacePressed = false;
+    }
+
+    if (eventType === "m" && text.length == 1 && isCalcButton.test(text)) {
+        memory += text;
+    }
+
+    if (eventType === "k" && isCalcButton.test(key)) {
+        memory += key;
+    }
+
+    if (isOperator.test(memory.charAt(memory.length - 1))) {
+        operatorPressed = true;
+    }
+
+    if (operatorPressed) {
+        display.textContent = "";
+        if (isOperator.test(memory.charAt(memory.length - 1))) {
+            memory.slice(0,-1);
+        } else if (isNumber.test(memory.charAt(memory.length - 1))) {
+            display.textContent += memory.charAt(memory.length - 1);
+        }
+        operatorPressed = false;
+    } else if (!operatorPressed) {
+       // console.log(display.textContent += memory.charAt(memory.length - 1));
+        if (isCalcButton.test(memory.charAt(memory.length - 1))) {
+            display.textContent += memory.charAt(memory.length - 1);
+        }
+    }
+
+    if (memory.charAt(memory.length - 1) === "=") {
+        memory = memory.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+        console.log(memory);
+        calculation();
+    }
 }
